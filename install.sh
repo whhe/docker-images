ce7b81c81bcea5fb52e69d02230fca73788dd745
He Wang <wanghechn@qq.com>
2024-12-09 04:19:31 +0800

    fix function

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ sudo sed -i "s/^root_pwd=\".*\"/root_pwd=\"$DATABASE_PASSWORD\"/" /etc/oceanbase
 sudo chown root /home/admin/oceanbase/etc
 sudo systemctl start oceanbase
 
-function wait_tenant_connectable() {
+function wait_tenant_connectable {
 	echo "check tenant connectable"
 	for i in {1..60}; do
 		if check_tenant_connectable; then
@@ -39,7 +39,7 @@ function wait_tenant_connectable() {
 	return 1
 }
 
-function check_tenant_connectable() {
+function check_tenant_connectable {
 	obclient -h127.0.0.1 -uroot -p123456 -e "SELECT svr_ip FROM oceanbase.__all_server"
 	return $?
 }
